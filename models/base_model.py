e208ac5ad4d1ae4dd4ac79f7d822145ec37d83e6
Mishel450 <tomasmishel18@gmai.com>
2023-07-05 07:01:18 -0700

    AirBnB_clone base_model

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,18 @@ class BaseModel:
 
     def __str__(self):
         """str"""
+
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
     def save(self):
         """save"""
+
         self.update_at = datetime.now()
 
     def to_dict(self):
         """to_dict"""
+
         td = self.__dict__.copy()
         td["__class__"] = str(type(self).__name__)
         td["update_at"] = self.update_at.isoformat()

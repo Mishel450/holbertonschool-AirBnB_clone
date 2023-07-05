13a41773e2db20bd136f93f5719e3ef2223daefa
Mishel450 <tomasmishel18@gmai.com>
2023-07-05 10:40:19 -0700

    AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """aaa"""
+from models.base_model import BaseModel
 
 
 class FileStorage:
     """FileStorage"""
 
     def __init__(self):
-        pass
+        self.__file_path = "file.json"
+        self.__objects = {}
 
+    def all(self):
+        return self.__objects
     
+    def new(self, obj):
+        self.__objects["{}.{}".format(__class__.__name__, BaseModel.id)] = obj
+        
+
+

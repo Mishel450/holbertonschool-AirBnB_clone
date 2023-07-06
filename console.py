6744a799bf4749c36d17198b79f5f99c3f3dba46
DarianGrabino <dariangra@gmail.com>
2023-07-06 00:53:37 -0700

    add create

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """console"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,6 +18,28 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """Do nothing on empty line"""
+        pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel"""
+        if not args:
+            print("** class name missing **")
+            return
+
+        name = args
+        clase = ["BaseModel"]
+
+        if name not in clase:
+            print("** class doesn't exist **")
+            return
+
+        if name == "BaseModel":
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def show(self, args):
+        """Prints the string of an instance based on the class name"""
         pass
 
 if __name__ == "__main__":

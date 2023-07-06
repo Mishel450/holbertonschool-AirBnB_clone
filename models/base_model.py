20d4f8c71cd4bedeba70469389c2d838d7728ef7
DarianGrabino <dariangra@gmail.com>
2023-07-05 23:47:02 -0700

    fix updated

--- models/base_model.py
+++ models/base_model.py
@@ -14,11 +14,11 @@ class BaseModel:
         if (kwargs):
             self.id = kwargs["id"]
             self.created_at = datetime.strptime(kwargs["created_at"], dformat)
-            self.update_at = datetime.strptime(kwargs["update_at"], dformat)
+            self.updated_at = datetime.strptime(kwargs["updated_at"], dformat)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.update_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """str"""
@@ -29,13 +29,13 @@ class BaseModel:
     def save(self):
         """save"""
 
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """to_dict"""
 
         td = self.__dict__.copy()
         td["__class__"] = str(type(self).__name__)
-        td["update_at"] = self.update_at.isoformat()
+        td["updated_at"] = self.updated_at.isoformat()
         td["created_at"] = self.created_at.isoformat()
         return (td)

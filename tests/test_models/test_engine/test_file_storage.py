1490e820e2dbd55c06f2083171b7a75e5d51eaee
DarianGrabino <dariangra@gmail.com>
2023-07-09 16:41:22 -0700

    delete test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,51 +8,10 @@ from models.base_model import BaseModel
 class FileStorageTest(unittest.TestCase):
     """test"""
 
-    def setUp(self):
-        self.fs = FileStorage()
-
     def test_file_path(self):
         fs = FileStorage()
         a = fs._FileStorage__file_path
         self.assertEqual(a, "file.json")
-
-    def test_all(self):
-        self.assertIsInstance(self.fs.all(), dict)
-
-    def test_all_2(self):
-        fs = FileStorage()
-        obj = fs.all()
-        self.assertIsNotNone(obj)
-        self.assertEqual(type(obj), dict)
-        self.assertIs(obj, fs._FileStorage__objects)
-
-    def test_all_object(self):
-        fs = FileStorage()
-        object = fs.all()
-        self.assertEqual(fs.all(), object)
-
-    def test_new(self):
-        bm = BaseModel()
-        self.fs.new(bm)
-        key = "{}.{}".format(type(bm).__name__, bm.id)
-        self.assertEqual(bm, self.fs.all()[key])
-
-    def test_save(self):
-        bm = BaseModel()
-        self.fs.new(bm)
-        self.fs.save()
-        with open(FileStorage._FileStorage__file_path, "r") as file:
-            file_content = file.read()
-        self.assertIn(type(bm).__name__, file_content)
-        self.assertIn(bm.id, file_content)
-
-    def test_reload(self):
-        bm = BaseModel()
-        self.fs.new(bm)
-        key = 'BaseModel.' + bm.id
-        self.fs.save()
-        self.fs.reload()
-        self.assertEqual(bm.to_dict(), self.fs.all()[key].to_dict())
 
     def test_everything(self):
         fs = FileStorage()
